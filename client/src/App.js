17696f5811bd49acd63a4f1b5c7fdea3d388bed5
Samantha Laine <samanthalaine8@gmail.com>
2021-12-10 01:51:58 -0500

    added cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,38 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     const getNotes = async () => {
-      await axios.get('http://localhost:5000/notes').then((res) => {
+      await axios.get("http://localhost:5000/notes").then((res) => {
         setNotes(res.data);
-      })
-    }
-    getNotes()
-  },[])
-
+      });
+    };
+    getNotes();
+  }, []);
 
   return (
     <>
-     <h1 className="text-center mt-5">Notes App</h1>
-      <form className="d-flex mt-5" >
-        <input
-          type="text"
-          className="form-control"
-          value
-          onChange
-        />
+      <h1 className="text-center mt-5">Notes App</h1>
+      <form className="d-flex mt-5">
+        <input type="text" className="form-control" value onChange />
         <button className="btn btn-primary">Add</button>
       </form>
-      {notes.map((note)=>{
-        return <h3>{note.description}</h3>
-      })}
+
+      <div class="row row-cols-1 row-cols-md-3 mt-3">
+        {notes.map((note) => {
+          return (
+            <div class="col mb-4">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-text">{note.description}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
