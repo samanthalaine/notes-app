1533036cc367151980cba9e3084c3f169f481e80
Samantha Laine <samanthalaine8@gmail.com>
2021-12-10 00:50:10 -0500

    installed axios & rendered notes onto page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    const getNotes = async () => {
+      await axios.get('http://localhost:5000/notes').then((res) => {
+        setNotes(res.data);
+      })
+    }
+    getNotes()
+  },[])
+
+
   return (
-    <div className="App">
-     hello world
-    </div>
+    <>
+     <h1 className="text-center mt-5">Notes App</h1>
+      <form className="d-flex mt-5" >
+        <input
+          type="text"
+          className="form-control"
+          value
+          onChange
+        />
+        <button className="btn btn-primary">Add</button>
+      </form>
+      {notes.map((note)=>{
+        return <h3>{note.description}</h3>
+      })}
+    </>
   );
 }
 
